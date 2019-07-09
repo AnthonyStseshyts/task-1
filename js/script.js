dfa29efebb56c7134f2029aa3d52ac1daac5b83b
Anthony Stseshyts <a.stseshyts@mail.ru>
2019-07-09 21:35:21 +0300

    Added a file

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function EventEm() {
+  this.events = {};
+}
+
+EventEm.prototype.on = function(eventName, fn) {
+  if (!this.events[eventName]) {
+    this.events[eventName] = [];
+  }
+  this.events[eventName].push(fn);
+
+  return () =>
+    (this.events[eventName] = this.events[eventName].filter(
+      functionEl => fn !== functionEl
+    ));
+};
+
+EventEm.prototype.emit = function(eventName, ...args) {
+  const event = this.events[eventName];
+  if (event) {
+    event.forEach(fn => {
+      fn(...args);
+    });
+  }
+};
+
+const ee = new EventEm();
+const unsubscribe = ee.on("hi", (name, surname) =>
+  console.log(`Hi, ${name} ${surname}`)
+);
+ee.emit("hi", "Anton", "Stseshyts");
+unsubscribe();
