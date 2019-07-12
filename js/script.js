348f9ea673d6aced66ea4a4f4d5b94c379b29b31
Anthony Stseshyts <a.stseshyts@mail.ru>
2019-07-12 10:09:56 +0300

    Added the second task in the sample

--- js/script.js
+++ js/script.js
@@ -34,3 +34,35 @@
 // ee.emit("hi", "Anton", "Stseshyts");
 // unsubscribe();
 
+// Sample of the task 2
+
+function B() {
+  this.counter = 0;
+}
+
+function inherit(parentFunc, data) {
+  let newObj = Object.create(parentFunc);
+
+  let childFunc = function() {
+    parentFunc.apply(this);
+  };
+
+  childFunc.prototype = Object.create(parentFunc);
+  for (let key in data) {
+    childFunc.prototype[key] = data[key];
+  }
+
+  return childFunc;
+}
+
+const F = inherit(B, {
+  construcor: function F() {
+    this.name = "foo";
+  },
+  getCounter: function() {
+    return this.counter;
+  }
+});
+
+const f = new F();
+console.log(f.getCounter()); // 0;
